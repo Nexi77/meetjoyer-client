0def0d864b0f71a93dfe00d889e8cfd8a2fa5e04
Konrad Kolasa <kolasakonrad1@gmail.com>
2024-07-23 22:06:25 +0200

    fix(bg-image): fix image size to look better

--- layouts/loggedOut.vue
+++ layouts/loggedOut.vue
@@ -8,6 +8,11 @@
 section {
     background-image: url('@images/bg_waves.svg');
     background-repeat: no-repeat;
-    background-size: cover;
+    background-size: 120% 150%;
+
+    @media screen and (width >= 740px) {
+        background-image: url('@images/bg_waves.svg');
+        background-size: 100% 100%;
+    }
 }
 </style>
